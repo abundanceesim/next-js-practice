152ab5e7771bd504200f20f846edd09a7953b54f
Abundance Esim <aesim@secan.com>
2025-02-26 12:56:58 -0500

    Add route handlers and implement basic REST api

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function About() {
+    // assign return value from routing to this to a const
+    const router = useRouter();
     return (
         <div>
             <h1>About Us</h1>
+            <button onClick={() => router.push('/')} className="bg-blue-500 text-white p-2 rouded-md">Go Home</button>
         </div>
     );
 }
